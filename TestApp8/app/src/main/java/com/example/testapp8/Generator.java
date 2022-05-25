389ed1aefd45488c996a6ebe8961c5c60b8a5f3a
podlesniy <podlesniy.1992@gmail.com>
2022-05-25 20:34:57 +0300

    ReComplete TestApp8 v3

--- TestApp8/app/src/main/java/com/example/testapp8/Generator.java
+++ TestApp8/app/src/main/java/com/example/testapp8/Generator.java
@@ -1,18 +1,13 @@
 package com.example.testapp8;
 
-
 import androidx.fragment.app.Fragment;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Generator extends Fragment {
 
-    private static ArrayList<String> nameList;
-    private static ArrayList<String> emailList;
-
     public static List<String> generateName() {
-        nameList = new ArrayList<>();
+        ArrayList<String> nameList = new ArrayList<>();
         nameList.add("Жена");
         nameList.add("Мама");
         nameList.add("Денис");
@@ -20,7 +15,7 @@ public class Generator extends Fragment {
     }
 
     public static List<String> generateEmail() {
-        emailList = new ArrayList<>();
+        ArrayList<String> emailList = new ArrayList<>();
         emailList.add("oksana@gmail.com");
         emailList.add("valentina@gmail.com");
         emailList.add("denchik@gmail.com");
@@ -29,12 +24,12 @@ public class Generator extends Fragment {
 
     public static String[] contactInfo(String name) {
         String[] contInf;
-        if (name.equals(nameList.get(0))) {
-            contInf = new String[]{nameList.get(0), "+380994311234", emailList.get(0), "Dnepr"};
-        } else if (name.equals(nameList.get(1))) {
-            contInf = new String[]{nameList.get(1), "+380955343456", emailList.get(1), "Mirnograd"};
-        } else if (name.equals(nameList.get(2))) {
-            contInf = new String[]{nameList.get(2), "+380501231234", emailList.get(2), "Mirnograd"};
+        if (name.equals(generateName().get(0))) {
+            contInf = new String[]{generateName().get(0), "+380994311234", generateEmail().get(0), "Dnepr"};
+        } else if (name.equals(generateName().get(1))) {
+            contInf = new String[]{generateName().get(1), "+380955343456", generateEmail().get(1), "Mirnograd"};
+        } else if (name.equals(generateName().get(2))) {
+            contInf = new String[]{generateName().get(2), "+380501231234", generateEmail().get(2), "Mirnograd"};
         } else {
             contInf = new String[]{"Извините","но","пока","не реализовано"};
         }
@@ -50,11 +45,11 @@ public class Generator extends Fragment {
     }
 
     public static Integer generateImage(String name) {
-        if (name.equals(nameList.get(0))) {
+        if (name.equals(generateName().get(0))) {
             return generateIcon().get(0);
-        } else if (name.equals(nameList.get(1))) {
+        } else if (name.equals(generateName().get(1))) {
             return generateIcon().get(1);
-        } else if (name.equals(nameList.get(2))) {
+        } else if (name.equals(generateName().get(2))) {
             return generateIcon().get(2);
         } else {
             return R.drawable.ic_launcher_foreground;
