97c2330dade04b627ea793fdd1f5b8ea4d0c2a6f
podlesniy <podlesniy.1992@gmail.com>
2022-06-12 09:51:44 +0300

    Add viewBinding in MainActivity2 in TestApp11

--- TestApp11/app/src/main/java/com/example/testapp11/MainActivity2.java
+++ TestApp11/app/src/main/java/com/example/testapp11/MainActivity2.java
@@ -2,22 +2,18 @@ package com.example.testapp11;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-import androidx.databinding.DataBindingUtil;
 import com.example.testapp11.databinding.ActivityMain2Binding;
 import java.util.Locale;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    TextView name;
     ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main2);
-        name = findViewById(R.id.name);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         binding.name.setText(MainActivity.countries.name);
         binding.capital.setText(MainActivity.countries.capital);
         binding.region.setText(MainActivity.countries.region);
