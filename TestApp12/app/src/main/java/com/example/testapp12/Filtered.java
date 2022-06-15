bae3a9c19c11fd68ed327d9626c9ec48c490fb9b
podlesniy <podlesniy.1992@gmail.com>
2022-06-15 23:34:47 +0300

    Add lambda in Filtered in TestApp12

--- TestApp12/app/src/main/java/com/example/testapp12/Filtered.java
+++ TestApp12/app/src/main/java/com/example/testapp12/Filtered.java
@@ -11,67 +11,58 @@ import java.util.List;
 public class Filtered {
 
     public static void filteredByName(List<Country> countries, EditText editText, Button button, List<Country> countryFiltered, CountryAdapter adapter) {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                countryFiltered.clear();
-                for (Country country : countries) {
-                    if (((!String.format("%s", country.name).equals("null")) && (country.name.toLowerCase().contains(editText.getText().toString())))
-                            || ((!String.format("%s", country.name).equals("null")) && (country.name.toUpperCase().contains(editText.getText().toString())))
-                            || (!String.format("%s", country.name).equals("null")) && (country.name.contains(editText.getText().toString())))
-                    {
-                        countryFiltered.add(country);
-                    }
+        button.setOnClickListener(view -> {
+            countryFiltered.clear();
+            for (Country country : countries) {
+                if (((!String.format("%s", country.name).equals("null")) && (country.name.toLowerCase().contains(editText.getText().toString())))
+                        || ((!String.format("%s", country.name).equals("null")) && (country.name.toUpperCase().contains(editText.getText().toString())))
+                        || (!String.format("%s", country.name).equals("null")) && (country.name.contains(editText.getText().toString())))
+                {
+                    countryFiltered.add(country);
                 }
-                adapter.update(countryFiltered);
-                if (editText.getText().toString().equals("")) {
-                    adapter.update(countries);
-                }
+            }
+            adapter.update(countryFiltered);
+            if (editText.getText().toString().equals("")) {
+                adapter.update(countries);
             }
         });
     }
 
     public static void filteredByCapital(List<Country> countries, EditText editText, Button button, List<Country> countryFiltered, CountryAdapter adapter) {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                countryFiltered.clear();
-                for (Country country : countries) {
-                    if (((!String.format("%s", country.capital).equals("null")) && (country.capital.toLowerCase().contains(editText.getText().toString())))
-                            || ((!String.format("%s", country.capital).equals("null")) && (country.capital.toUpperCase().contains(editText.getText().toString())))
-                            || (!String.format("%s", country.capital).equals("null")) && (country.capital.contains(editText.getText().toString())))
-                    {
-                        countryFiltered.add(country);
-                    }
+        button.setOnClickListener(view -> {
+            countryFiltered.clear();
+            for (Country country : countries) {
+                if (((!String.format("%s", country.capital).equals("null")) && (country.capital.toLowerCase().contains(editText.getText().toString())))
+                        || ((!String.format("%s", country.capital).equals("null")) && (country.capital.toUpperCase().contains(editText.getText().toString())))
+                        || (!String.format("%s", country.capital).equals("null")) && (country.capital.contains(editText.getText().toString())))
+                {
+                    countryFiltered.add(country);
                 }
-                adapter.update(countryFiltered);
-                if (editText.getText().toString().equals("")) {
-                    adapter.update(countries);
-                }
+            }
+            adapter.update(countryFiltered);
+            if (editText.getText().toString().equals("")) {
+                adapter.update(countries);
             }
         });
     }
 
     public static void filteredByPopulation(List<Country> countries, EditText editText, Button button, List<Country> countryFiltered, CountryAdapter adapter) {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                countryFiltered.clear();
-                double pop = 0;
-                if (!editText.getText().toString().isEmpty()) {
-                    pop = Double.parseDouble(editText.getText().toString());
+        button.setOnClickListener(v -> {
+            countryFiltered.clear();
+            double pop = 0;
+            if (!editText.getText().toString().isEmpty()) {
+                pop = Double.parseDouble(editText.getText().toString());
+            }
+            for (Country country : countries) {
+                if ((country.population >= pop - ((pop * 15.0) / 100))
+                        && (country.population <= pop + ((pop * 15.0) / 100)))
+                {
+                    countryFiltered.add(country);
                 }
-                for (Country country : countries) {
-                    if ((country.population >= pop - ((pop * 15.0) / 100))
-                            && (country.population <= pop + ((pop * 15.0) / 100)))
-                    {
-                        countryFiltered.add(country);
-                    }
-                }
-                adapter.update(countryFiltered);
-                if (editText.getText().toString().equals("")) {
-                    adapter.update(countries);
-                }
+            }
+            adapter.update(countryFiltered);
+            if (editText.getText().toString().equals("")) {
+                adapter.update(countries);
             }
         });
     }
